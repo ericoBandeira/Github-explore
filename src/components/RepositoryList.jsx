763d30e0441f3ce05424cf9d1831610ee48aa764
ericoBandeira <ericobandeira@hotmail.com>
2021-03-13 18:38:32 -0300

    Adicionando estilização e alimentando lista com github

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,24 +1,30 @@
 import { RepositoryItem } from "./RepositoryItem";
+import {useState,useEffect} from 'react';
+import '../styles/repositories.scss';
+
+
 
 export function RepositoryList(){
 
-    const repository= {
-        name:'unform',
-        description:"forms in React",
-        link:"https://github.com.br/unform/unform"
-    }
+    //https://api.github.com/users/ericoBandeira/repos
+
+    const [repositories,setRepositories] = useState([]);
+
+    useEffect(()=>{
+       fetch('https://api.github.com/users/ericoBandeira/repos')
+        .then(response=> response.json())
+        .then(data => setRepositories(data)) 
+    }, []); //n esquecer o segundo parâmetro 
 
 
     return(
-        <section>
+        <section className="repository-list">
             <h1>Lista de repositórios</h1>
             <ul>
-               <RepositoryItem 
-                repository={repository}
-                />
-               <RepositoryItem repository={repository}/>
-               <RepositoryItem repository={repository}/>
-               <RepositoryItem repository={repository}/>
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository}/>
+                })}
+               
             </ul>
         </section>
     );
